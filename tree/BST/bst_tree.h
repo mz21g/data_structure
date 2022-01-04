dc7d90f4a7c7a55cde8926ebcab1b3712b46cef6
yzChen <2496565065@qq.com>
2022-01-04 10:04:05 +0800

    update

--- tree/BST/bst_tree.h
+++ tree/BST/bst_tree.h
@@ -4,5 +4,28 @@
 
 #ifndef DATA_STRUCTURE_BST_TREE_H
 #define DATA_STRUCTURE_BST_TREE_H
+#include <iostream>
+
+// 二叉搜索树的构造
+struct BST {
+    int key;
+    BST *left, *parent, *right;
+};
+
+// 将关键字插入到BST中
+int insert(BST &T, int k, BST* parent=nullptr);
+
+// 构造BST
+void creat(BST &t, int arr[], int n);
+
+// 搜索指定关键字对应节点
+BST* search(BST &T, int k);
+
+// 搜索关键字最小的节点
+BST* bst_minimum(BST &t);
+
+// 搜索关键字最大的节点
+BST* bst_maximum(BST &t);
+
 
 #endif //DATA_STRUCTURE_BST_TREE_H
